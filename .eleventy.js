52e28555d0e0555a91ede8d8feba6f1a949576ee
Kevin Powell <hi@kevinpowell.co>
2021-03-12 15:08:38 -0500

    update redirects

--- .eleventy.js
+++ .eleventy.js
@@ -31,6 +31,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/admin");
